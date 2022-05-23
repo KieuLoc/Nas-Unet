dde2bfcc8ef293857be51f80b8695a71fb8f6ddf
KieuLoc <kieuxuanloc121200@gmail.com>
2022-05-23 23:39:01 +0700

    sklearn split data

--- util/datasets/pasval_voc_lockx.py
+++ util/datasets/pasval_voc_lockx.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 from PIL import Image, ImageOps, ImageFilter
 from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
 import torch
 from .base import BaseDataset
@@ -27,19 +28,25 @@ class VOCLocKxSegmentation(BaseDataset):
         labels_path = os.path.join(_voc_root, 'labels_train.npz')
         m_train_dataset = np.load( self.INPUT + '/seismic-facies/data_train.npz', allow_pickle=True, mmap_mode='r')['data']
         m_train_labels = np.load(self.INPUT + '/seismic-facies/labels_train.npz', allow_pickle=True, mmap_mode='r')['labels']
-        m_test_dataset = np.load(self.INPUT + '/seismic-facies/data_test_1.npz', allow_pickle=True, mmap_mode='r')['data']
-        m_test_labels = np.load( self.INPUT + '/seimic-data/sample_submission_1.npz', allow_pickle=True, mmap_mode='r')['prediction']
+        #m_test_dataset = np.load(self.INPUT + '/seismic-facies/data_test_1.npz', allow_pickle=True, mmap_mode='r')['data']
+        #m_test_labels = np.load( self.INPUT + '/seimic-data/sample_submission_1.npz', allow_pickle=True, mmap_mode='r')['prediction']
         #m_train_dataset, m_test_dataset, m_train_labels, m_test_labels = train_test_split(
         #m_train_dataset, m_train_labels, test_size = 0.2, random_state = 42)
+        m_train_dataset1, m_test_dataset, m_train_labels1, m_test_labels = train_test_split(
+            m_train_dataset, m_train_labels, test_size=0.2, random_state=42)
         print("Run NAS UNet from LOC KX")
         self.joint_transform = None
 
         if self.mode == 'train':
             self.train_dataset = m_train_dataset
             self.train_labels = m_train_labels
+            print(f"train size: +data={len(self.train_dataset)}, label={len(self.train_labels)}")
+            print(f"test  size: + data={len(m_test_dataset)}, label={len(m_test_labels)}")
         elif self.mode == 'val':
             self.train_dataset = m_test_dataset
             self.train_labels = m_test_labels
+            print(f"train size: + data={len(m_train_dataset1)}, label={len(m_train_labels1)}")
+            print(f"test  size: + data={len(m_test_dataset)}, label={len(m_test_labels)}")
         # elif self.mode == 'test':
         #     self.images = []
         #     return
