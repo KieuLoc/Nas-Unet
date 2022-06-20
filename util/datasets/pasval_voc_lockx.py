0b9917cb52413411d2ae109c0248c49659b90338
KieuLoc <kieuxuanloc121200@gmail.com>
2022-06-20 21:04:54 +0700

    [Split data] sklearn

--- util/datasets/pasval_voc_lockx.py
+++ util/datasets/pasval_voc_lockx.py
@@ -30,8 +30,8 @@ class VOCLocKxSegmentation(BaseDataset):
         m_train_labels = np.load(self.INPUT + '/seismic-facies/labels_train.npz', allow_pickle=True, mmap_mode='r')['labels']
         #m_test_dataset = np.load(self.INPUT + '/seismic-facies/data_test_1.npz', allow_pickle=True, mmap_mode='r')['data']
         #m_test_labels = np.load( self.INPUT + '/seimic-data/sample_submission_1.npz', allow_pickle=True, mmap_mode='r')['prediction']
-        # m_train_dataset, m_test_dataset, m_train_labels, m_test_labels = train_test_split(
-        # m_train_dataset, m_train_labels, test_size = 0.2, random_state = 42)
+        m_train_dataset, m_test_dataset, m_train_labels, m_test_labels = train_test_split(
+        m_train_dataset, m_train_labels, test_size = 0.2, random_state = 42)
 
         print("Run NAS UNet from LOC KX")
         self.joint_transform = None
@@ -42,11 +42,11 @@ class VOCLocKxSegmentation(BaseDataset):
             print(f"train sizes: +data={len(self.train_dataset)}, label={len(self.train_labels)}")
             print(f"test sizes: + data={len(m_test_dataset)}, label={len(m_test_labels)}")
         elif self.mode == 'val':
-            # self.train_dataset = m_test_dataset
-            # self.train_labels = m_test_labels
-            # print(f"train val size: + data={len(m_test_dataset)}, label={len(m_test_labels)}")
-            # print(f"test val size: + data={len(m_test_dataset)}, label={len(m_test_labels)}")
-            print(f"hê")
+            self.train_dataset = m_test_dataset
+            self.train_labels = m_test_labels
+            print(f"train val size: + data={len(m_test_dataset)}, label={len(m_test_labels)}")
+            print(f"test val size: + data={len(m_test_dataset)}, label={len(m_test_labels)}")
+            # print(f"hê")
         # elif self.mode == 'test':
         #     self.images = []
         #     return
