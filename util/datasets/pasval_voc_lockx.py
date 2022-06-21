e729c1d2e6b6a0c67b155dfe056b0b893e41a8c2
KieuLoc <kieuxuanloc121200@gmail.com>
2022-06-21 16:24:13 +0700

    test1

--- util/datasets/pasval_voc_lockx.py
+++ util/datasets/pasval_voc_lockx.py
@@ -30,15 +30,15 @@ class VOCLocKxSegmentation(BaseDataset):
         m_train_labels = np.load(self.INPUT + '/seismic-facies/labels_train.npz', allow_pickle=True, mmap_mode='r')['labels']
         #m_test_dataset = np.load(self.INPUT + '/seismic-facies/data_test_1.npz', allow_pickle=True, mmap_mode='r')['data']
         #m_test_labels = np.load( self.INPUT + '/seimic-data/sample_submission_1.npz', allow_pickle=True, mmap_mode='r')['prediction']
-        m_train_dataset, m_test_dataset, m_train_labels, m_test_labels = train_test_split(
+        m_train_dataset1, m_test_dataset, m_train_labels1, m_test_labels = train_test_split(
         m_train_dataset, m_train_labels, test_size = 0.2, random_state = 42)
 
         print("Run NAS UNet from LOC KX")
         self.joint_transform = None
 
         if self.mode == 'train':
-            self.train_dataset = m_train_dataset
-            self.train_labels = m_train_labels
+            self.train_dataset = m_train_dataset1
+            self.train_labels = m_train_labels1
             print(f"train sizes: +data={len(self.train_dataset)}, label={len(self.train_labels)}")
             print(f"test sizes: + data={len(m_test_dataset)}, label={len(m_test_labels)}")
         elif self.mode == 'val':
