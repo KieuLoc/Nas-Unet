5be5f119628841e0927f6497e27b8c3d705c45a4
KieuLoc <kieuxuanloc121200@gmail.com>
2022-07-28 23:44:21 +0700

    Append data

--- util/datasets/pasval_voc_lockx.py
+++ util/datasets/pasval_voc_lockx.py
@@ -28,8 +28,7 @@ class VOCLocKxSegmentation(BaseDataset):
         labels_path = os.path.join(_voc_root, 'labels_train.npz')
         m_train_dataset = np.load( self.INPUT + '/seismic-facies/data_train.npz', allow_pickle=True, mmap_mode='r')['data']
         m_train_labels = np.load(self.INPUT + '/seismic-facies/labels_train.npz', allow_pickle=True, mmap_mode='r')['labels']
-        #m_test_dataset = np.load(self.INPUT + '/seismic-facies/data_test_1.npz', allow_pickle=True, mmap_mode='r')['data']
-        #m_test_labels = np.load( self.INPUT + '/seimic-data/sample_submission_1.npz', allow_pickle=True, mmap_mode='r')['prediction']
+
         m_train_dataset1, m_test_dataset, m_train_labels1, m_test_labels = train_test_split(
         m_train_dataset, m_train_labels, test_size = 0.2, random_state = 42)
 
@@ -73,36 +72,38 @@ class VOCLocKxSegmentation(BaseDataset):
         img = (img * 255).astype(int)
         img = img / 255.
         #-----------------------------------------------------
-        # img2 = self.train_dataset[:, :, index]
+        img2 = self.train_dataset[:, :, index]
         # label2 = self.train_labels[:, :, index]
         #
-        # img2 = np.expand_dims(img2, axis=2).astype('float32')
+        img2 = np.expand_dims(img2, axis=2).astype('float32')
         # label2 = np.expand_dims(label2, axis=2).astype('float32')
         #
-        # img2 = cv2.resize(img2, (128, 128))
+        img2 = cv2.resize(img2, (128, 128))
         # label2 = cv2.resize(label2, (128, 128))
         #
-        # img2 = img2 / np.amax(img2)
-        # img2 = np.clip(img2, 0, 255)
-        # img2 = (img2 * 255).astype(int)
-        # img2 = img2 / 255.
+        img2 = img2 / np.amax(img2)
+        img2 = np.clip(img2, 0, 255)
+        img2 = (img2 * 255).astype(int)
+        img2 = img2 / 255.
         # #-----
-        # img1 = self.train_dataset[:, index, :]
+        img1 = self.train_dataset[:, index, :]
         # label1 = self.train_labels[:, index, :]
         #
-        # img1 = np.expand_dims(img1, axis=2).astype('float32')
+        img1 = np.expand_dims(img1, axis=2).astype('float32')
         # label1 = np.expand_dims(label1, axis=2).astype('float32')
         #
-        # img1 = cv2.resize(img1, (128, 128))
+        img1 = cv2.resize(img1, (128, 128))
         # label1 = cv2.resize(label1, (128, 128))
         #
-        # img1 = img1 / np.amax(img1)
-        # img1 = np.clip(img1, 0, 255)
-        # img1 = (img1 * 255).astype(int)
-        # img1 = img1 / 255.
+        img1 = img1 / np.amax(img1)
+        img1 = np.clip(img1, 0, 255)
+        img1 = (img1 * 255).astype(int)
+        img1 = img1 / 255.
         # #-----------------------------------------------------
-        img = np.append(np.append([img], [img], axis=0), [img], axis=0)
-        # img = np.append(np.append([img2], [img2], axis=0), [img2], axis=0)
+        #img = np.append(np.append([img], [img], axis=0), [img], axis=0)
+        img = np.append(img1)
+        img = np.append(img2)
+        #img = np.append(np.append([img2], [img2], axis=0), [img2], axis=0)
         #
         # label = np.append(np.append([label1], [label1], axis=0), [label1], axis=0)
         # label = np.append(np.append([label2], [label2], axis=0), [label2], axis=0)
